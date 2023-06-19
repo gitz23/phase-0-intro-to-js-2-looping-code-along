e5137fdd80f5d9d3918f67790be4bed29b82c6b8
Gitau Njung'e <gitau.njunge@student.moringaschool.com>
2023-06-19 14:21:03 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"]
+//const event = 'surprise'
+
+function writeCards(names, event){
+    const newArray= [];
+    for (let i = 0; i < names.length; i++){
+        let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+        newArray.push(message);
+    }
+    return newArray;
+}
+console.log(writeCards(names, 'surprise'))
+
+function countDown(){
+    let countDown= 0
+    while(countDown < 11){
+        console.log(countDown++)
+    }
+}
